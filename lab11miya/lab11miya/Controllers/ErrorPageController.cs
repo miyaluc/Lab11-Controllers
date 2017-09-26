7162c7c992f052e8df6d5b8c3062ca25a4b1237e
miyaluc <srlucas3@gmail.com>
2017-09-26 12:53:22 -0700

    readme updated added controllers, routing, custom routing, action methods, interpolation

---
+++ lab11miya/lab11miya/Controllers/ErrorPageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab11miya.Controllers
+{
+    [Route("Error")]
+    public class ErrorPageController
+    {
+        public string Error()
+        {
+            return "Oops. Something's not right....";
+        }
+    }
+}
---
+++ lab11miya/lab11miya/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab11miya.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "You are at the home page.";
+        }
+        public string Options()
+        {
+            return "Familiarize yourself with the site. This will be your primary student portal.";
+        }
+    }
+}
